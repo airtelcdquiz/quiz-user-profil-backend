4adaba98417b523f1059e5f4495387e96b5cfcd6
airtelcdquiz <airtel.cd.quiz@gmail.com>
2026-02-18 17:30:47 +0000

    fixe School is not defined

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const School = require('../models/School')
 const { enqueueSMS } = require('../lib/smsQueue')
 
 // GET /users/:phoneNumber
